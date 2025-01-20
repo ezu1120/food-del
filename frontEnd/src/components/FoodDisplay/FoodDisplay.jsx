60650dc791a53dd22d3719f4f94aa831a7b6bc00
ezu1120 <laleezu1220@gmail.com>
2025-01-20 10:57:36 +0300

    Update FoodDisplay.jsx

--- frontEnd/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontEnd/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import  { useContext } from "react";
 import "./FoodDisplay.css";
-import { StoreContext } from "../context/StoreContext";
+import { StoreContext } from "../Context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext); // Access the food list from context
