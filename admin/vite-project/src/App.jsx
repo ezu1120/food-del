0fcbb22961700e350b86c244a8c9ab49c5248fd4
ezu1120 <laleezu1220@gmail.com>
2025-01-20 15:12:04 +0300

    Update App.jsx

--- admin/vite-project/src/App.jsx
+++ admin/vite-project/src/App.jsx
@@ -9,7 +9,7 @@ import Orders from "./pages/Orders/Orders";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const App = () => {
-  const url = "http://localhost:4000"
+  const url = "https://food-del-backend-hnvd.onrender.com"
   return (
     <div>
       <ToastContainer />
